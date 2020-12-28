16bbe460f46da3929a884a7c8f3e441d1f21b708
Zoia Ivanova <zoia11ivanova@gmail.com>
2020-12-28 15:49:43 +0200

    Refactoring and renaming funtions

--- fn_luckytickets.py
+++ fn_luckytickets.py
@@ -2,22 +2,27 @@ import re
 
 
 def main():
-    menu_prompt = """
-    Lucky tickets: there are 2 ways to count lucky tickets.\n
-    The first one is simple (sum of first 3 numbers equals sum of second 3 numbers).
-    The second method is complex (sum of even numbers equals sum of odd numbers).\n
-    You need to enter minimum and maximum number of tickets. 
-    The output of the program will be the info about method that has won and the number of lucky tickets in range of
-    your min amd max number.
-
+    program_options = """
     Please enter one of the following options:
 
     - (press any key) to enter min and max ticket number
     - 'q' to quit
+    """
+    menu_prompt_start = f"""
+    Lucky tickets: there are 2 ways to count lucky tickets.\n
+    The first one is simple (sum of first 3 numbers equals sum of second 3 numbers).
+    The second method is complex (sum of even numbers equals sum of odd numbers).\n
+    You need to enter minimum and maximum number of tickets.
+    The output of the program will be the info about method that has won and the number of lucky tickets in range of
+    your min amd max number.
+    {program_options}
+    """
+    menu_prompt_during_program = f"""
+    Would you like to run this program one more time?
+    {program_options}
+    """
 
-    What would you like to do? """
-
-    selected_option = input(menu_prompt).strip().lower()
+    selected_option = input(menu_prompt_start).strip().lower()
 
     while selected_option != 'q':
         try:
@@ -31,23 +36,28 @@ def main():
             print(error)
         else:
             print(compete_lucky_ticket_counting_functions(min_number, max_number))
-
-        # Allow the user to change their selection at the end of each iteration
-        selected_option = input(menu_prompt).strip().lower()
+        finally:
+            selected_option = input(menu_prompt_during_program).strip().lower()
 
 
 def validate_ticket(ticket: str) -> None:
     '''
-    :param ticket: string input by user for validation
-    :return: True if ticket number has 6 digits otherwise False
+    :param ticket: user input string for validation
+    :return: raise ValueError if ticket isn't 6-digit string otherwise return None
     '''
     if not (len(ticket) == 6 and re.match(r'^[0-9]+$', ticket)):
         raise ValueError(f'Your value {ticket} is invalid, a ticket should consist of 6 digits')
 
 
 def validate_ticket_range(min_number: str, max_number: str) -> None:
+    '''
+
+    :param min_number: user input string as min number
+    :param max_number: user input string as max number
+    :return: raise ValueError if min number is greater than max number otherwise None
+    '''
     if int(min_number) > int(max_number):
-        raise ValueError(f'You entered invalid range of tickets: your min ticket is greater than max ticket')
+        raise ValueError('You entered invalid range of tickets: your min ticket is greater than max ticket')
 
 
 def is_simple_lucky(ticket: str) -> bool:
@@ -55,7 +65,7 @@ def is_simple_lucky(ticket: str) -> bool:
     :param ticket: valid 6-digit string
     :return: True if sum of first digits equals sum of last 3 digits otherwise False
     '''
-    ticket = [int(n) for n in ticket]
+    ticket = [int(digit) for digit in ticket]
     return sum(ticket[3:]) == sum(ticket[:3])
 
 
@@ -64,12 +74,12 @@ def is_complex_lucky(ticket: str) -> bool:
     :param ticket: valid 6-digit string
     :return: True if sum of first digits equals sum of last 3 digits otherwise False
     '''
-    odd = sum([int(n) for n in ticket if int(n) % 2 != 0])
-    even = sum([int(n) for n in ticket if int(n) % 2 == 0])
+    odd = sum([int(digit) for digit in ticket if int(digit) % 2 != 0])
+    even = sum([int(digit) for digit in ticket if int(digit) % 2 == 0])
     return odd == even
 
 
-def convert_to_string_with_zeros(number: int) -> str:
+def convert_to_string_with_prepended_zeros(number: int) -> str:
     '''
     :param number: number from a given range of min and max numbers
     :return: 6-digit string prepended with zeros if input number < 100000
@@ -90,7 +100,7 @@ def get_valid_tickets(min_number: str, max_number: str) -> list:
     :return: list of 6-digit ticket strings (prepended with zeros when number is less than 100000)
     in range of min and max number
     '''
-    return [convert_to_string_with_zeros(number) for number in range(int(min_number), int(max_number) + 1)]
+    return [convert_to_string_with_prepended_zeros(number) for number in range(int(min_number), int(max_number) + 1)]
 
 
 def compete_lucky_ticket_counting_functions(min_number: str, max_number: str) -> str:
